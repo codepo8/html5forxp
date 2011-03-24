ad90685901a0e3a3881d4a50995b62621ca6a916
Chris Heilmann <chris.heilmann@gmail.com>
2011-03-24 09:49:05 +0000

    eh voila

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>HTML5 for Windows XP - Upgrade from Internet Explorer</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<link rel="stylesheet" href="styles.css" type="text/css" media="screen" charset="utf-8">
+</head>
+<body>
+  
+<div class="header"><header><h1>HTML5 for XP now!</h1></header></div>
+
+<div class="container">
+
+<div class="article"><article>
+  
+<p class="intro">This is a call to arms to make Windows XP users move away from Internet Explorer to help us all make the web a richer and more enjoyable place.</p>  
+  
+<p>The internet already is a wonderful place. A media for all and a place to find everything you need. We need to care for it, advance it as a media and use it to its full potential much like we do with the other media we have.</p>
+
+<p>Over the years, the internet and our use of it has grown. We want more than just some texts with pretty images and forms to fill in and to send off. To make the web easy to use for everybody, we need to upgrade the infrastructure of the web and the software we use to consume it.</p>
+
+<p>Much like you need to have a straight and even road to drive fast with your car and use the least petrol, you need modern browsers to get the best out of the web of today.</p>
+
+<p>Microsoft <a href="http://www.beautyoftheweb.com/">promote a beautiful web</a> with the release of Internet Explorer 9 and urge people to expect more of their day-to-day web experience but as IE9 is only available for Windows Vista and Windows 7 users, those who use Windows XP (as it is good enough for them) can not get a beautiful web.</p>
+
+<p>Windows XP users can only upgrade to IE8 as a replacement for the woefully outdated IE6, which - even according to Microsoft - <a href="http://ie6countdown.com/">needs to go away</a>.</p>
+
+<p><strong>This is not fair and this is not helping the web.</strong></p> 
+
+<p>Windows XP users should turn their back on Internet Explorer and choose a better browser.</p>
+
+<h2>An option to fix your current browser?</h2>
+
+<p>If you can not install a different browser on your system - take a look at <a href="http://code.google.com/chrome/chromeframe/">Google's Chrome Frame</a> to at least patch it.</p>
+
+
+<h2>What we can do with better browsers</h2>
+
+<p>HTML5 and related technologies are the <strong>evolution of the web as we know it</strong>. Instead of using lots of meaningless code to simulate what browsers should do for us, we have new elements and powerful systems to use to make the web a more interesting and easier to use place. Just think of the things we can already do:</p>
+
+<ul><li>Native video and audio with keyboard enabled controls and the option to be manipulated in any way necessary.</li><li>Forms with richer controls like colour pickers, calendars and autocomplete fields.</li><li>Rounded corners, gradients and drop shadows without the need for any graphics</li><li>Local and offline storage to create real application experiences</li><li>Using geographical location and device orientation to give you a personalised experience of the web</li><li>Allowing for rich graphs and visualisations using Canvas and SVG.</li><li>Creating games that don't ask you to install plugins.</li><li>3D graphics and support for any font</li><li>Drag and drop and touch support for tablets.</li><li>File upload and access to the file system like any other application.</li></ul>
+
+<p>All this can be yours - just move on from Internet Explorer if you stick with XP.</p>
+
+
+
+</article></div>
+
+<div class="aside"><aside>
+<h1>Make the web better with a new browser!</h1>  
+<p>If you are an XP user and you can't or don't want to upgrade to Vista or Windows 7,change from Internet Explorer to a browser that supports modern technologies. Please download any of the following:</p>
+
+<nav><ul class="downloads">
+<?php 
+$links=array(
+'<li><a href="http://www.mozilla.com/en-US/firefox/all.html">Firefox</a></li>',
+  '<li><a href="http://www.google.com/chrome/">Chrome</a></li>',
+  '<li><a href="http://www.opera.com/browser">Opera</a></li>',
+  '<li><a href="http://www.apple.com/safari/">Safari</a></li>');
+$link = shuffle($links);
+echo join($links);
+?>
+</ul></nav>
+</aside></div>
+
+</div>
+
+<div class="footer"><footer>
+  <nav><ul><li><strong>English</strong></li><li><a href="index_de.php">Deutsch</a></li><li><a href="index_nl.php">Nederlands</a></li></ul></nav>
+  
+  <p>Written by <a href="http://twitter.com/codepo8">Chris Heilmann</a>, powered by annoyance and the memory of hours wasted on IE6 trying to replicate bugs instead of playing with new, cool technology.</p>
+  <a href="http://www.w3.org/html/logo/">
+    <img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="165" height="64" alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics">
+    </a>
+  
+</footer></div>
+<!-- Start of StatCounter Code -->
+<script type="text/javascript">
+var sc_project=702220; 
+var sc_invisible=1; 
+var sc_security="92a002ea"; 
+</script>
+
+<script type="text/javascript"
+src="http://www.statcounter.com/counter/counter.js"></script><noscript><div
+class="statcounter"><a title="vBulletin analytics"
+href="http://statcounter.com/vbulletin/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/702220/0/92a002ea/1/"
+alt="vBulletin analytics" ></a></div></noscript>
+<!-- End of StatCounter Code -->
+</body>
+</html>
---
+++ index_nl.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="de"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="UTF-8">
+  <title>HTML5 voor XP nu!</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<link rel="stylesheet" href="http://www.html5forxp.com/styles.css" type="text/css" media="screen" charset="utf-8">
+</head>
+<body>
+  
+<div class="header"><header><h1>HTML5 voor XP nu!</h1></header></div>
+
+<div class="container">
+
+<div class="article"><article>
+  
+  
+ 
+<p class="intro">Dit is een oproep aan alle Windows XP gebruikers om 
+geen gebruik meer te maken van Internet Explorer zodat we samen van het 
+internet een betere en meer genietbare plaats kunnen maken.</p> 
+ 
+<p>Het internet is een wonderlijke plaats, een medium voor iedereen en 
+een plek om alles te vinden wat je nodig hebt. We moeten er zorg voor 
+dragen, het uitbreiden als een medium en er ten volle gebruik van maken 
+net zoals we met andere media doen.</p> 
+ 
+<p>Door de jaren heen is het het internet en ons gebruik ervan 
+veranderd. Tegenwoordig willen we meer dan enkel maar teksten met mooie 
+afbeeldingen en formulieren die we kunnen invullen en verzenden. Om het 
+web gemakkelijker te maken voor iedereen moeten we daarom de 
+infrastructuur van het internet en de software die we er voor gebruiken
+upgraden.</p> 
+ 
+<p>Net zoals een auto het beste werkt op een vlakke rechte weg om op 
+zijn best te kunnen presenteren, vereist het huidige internet een 
+moderne browser om het beste uit het web te halen.</p> 
+ 
+<p>Microsoft promoot "<a href="http://www.beautyoftheweb.com/">een mooier web</a>"
+ met het uitbrengen van Internet Explorer 9 en zet mensen aan om meer 
+van de dagelijkse web-ervaringen te eisen. Helaas is IE9 enkel 
+beschikbaar voor Windows Vista en Windows 7 gebruikers, waardoor Windows
+ XP gebruikers (wat voor hen goed genoeg is) niet kunnen genieten van 
+een mooier web.</p> 
+ 
+<p>Windows XP gebruikers kunnen enkel updaten naar IE8 als een 
+vervanging voor het totaal verouderde IE6, dat – zelfs volgens Microsoft
+ – <a href="http://ie6countdown.com/">volledig afgeschaft moet worden</a>.</p> 
+ 
+
+<p><strong>Dit is niet eerlijk en helpt de modernisering van het web niet.</strong></p> 
+ 
+<p>Windows XP gebruikers moeten hun rug keren naar Internet Explorer en kiezen voor een betere browser.</p> 
+ 
+<h2>Een optie om je huidige Internet Explorer browser te verbeteren?</h2> 
+
+<p>Als je geen andere browser kan installeren op je systeem – neem dan tenminste een kijkje naar <a href="http://code.google.com/chrome/chromeframe/">Google Chrome Frame</a> om je browser in ieder geval een beetje beter te laten werken.</p> 
+
+<h2>Wat kunnen we doen met betere browsers?</h2> 
+ 
+<p>HTML5 en gerelateerde technologieën zijn <strong>de evolutie van het huidige web</strong>.
+ In plaats van veel betekenisloze code te gebruiken die aangeven wat 
+browsers voor ons zouden moeten doen, hebben we nieuwe elementen en 
+betere methodes om van het web een beter en gemakkelijker systeem te 
+maken. Hier zijn een paar dingen die we nu al kunnen doen:</p> 
+ 
+
+<ul>
+<li>Ingebouwde video and audio met controle via het toetsenbord en de 
+mogelijkheid om manipulaties toe te voegen in elke manier mogelijk.</li>
+
+<li>Formulieren met meer mogelijkheden zoals kleurkiezers, kalenders en auto-invulvelden.</li>
+
+<li>Afgeronde hoeken, kleurovergangen en schaduwen, zonder dat er afbeeldingen gebruikt hoeven te worden.</li>
+
+<li>Lokale en offline informatie opslag om echte desktop applicatie-ervaringen te creëren.</li>
+
+<li>Geografische locatie en toestel oriëntatie informatie om je een gepersonaliseerde web ervaring te bieden.</li>
+
+<li>Krachtige grafieken en visualisaties door het gebruik van Canvas en SVG.</li>
+
+<li>Games die geen plugin hoeven te installeren om te kunnen werken.</li>
+
+<li>3D visualisaties en ingebouwde lettertype ondersteuning.</li>
+
+<li>Drag and drop en touch ondersteuning voor mobiele tabletten.</li>
+
+<li>Bestand uploads en toegang tot het bestanden beheer zoals elke andere applicatie dat kan.</li>
+</ul>
+
+<p>Al het bovenstaande kan van jouw zijn, overstappen van Internet 
+Explorer naar een andere browser is het enige wat je hoeft te doen als je XP wil blijven 
+gebruiken.</p> 
+ 
+</article></div> 
+ 
+
+<div class="aside"><aside> 
+  <h1>Verbeter het web met een nieuwe browser!</h1>  
+  <p>Als je een Windows XP gebruiker bent en niet kan of wil opwaarderen
+ naar Vista of Windows 7, stap dan over van Internet Explorer naar een 
+browser die moderne technologieën ondersteunt. Download daarvoor één van
+ de volgende browsers:</p> 
+ 
+<nav><ul class="downloads"> 
+  <li><a href="http://www.mozilla.com/en-US/firefox/all.html">Firefox</a></li><li><a href="http://www.apple.com/safari/">Safari</a></li><li><a href="http://www.google.com/chrome/">Chrome</a></li><li><a href="http://www.opera.com/browser">Opera</a></li></ul></nav> 
+</aside></div> 
+ 
+</div> 
+ 
+<div class="footer"><footer> 
+  <nav><ul><li><a href="http://www.html5forxp.com/index.php">English</a></li><li><a href="http://www.html5forxp.com/index_de.php">Deutsch</a></li><li><strong>Nederlands</strong></li></ul></nav> 
+  
+  <p>Geschreven door <a href="http://twitter.com/codepo8">Chris Heilmann</a>,
+ gedreven door frustratie en herinneringen aan de vele verspilde uren aan foutopsporingen in IE6, in plaats van met nieuwe leuke technologieën te spelen. 
+Nederlandstalige versie door <a href="http://twitter.com/desloovere_j">Jeroen Desloovere</a>, <a href="http://twitter.com/kurafire">Faruk Ates</a> and <a href="http://twitter.com/cbetta">Cristiano Betta</a></p> 
+    <a href="http://www.w3.org/html/logo/">
+
+    <img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="165" height="64" alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics">
+    </a>
+  
+</footer></div>
+<!-- Start of StatCounter Code -->
+<script type="text/javascript">
+var sc_project=702220; 
+var sc_invisible=1; 
+var sc_security="92a002ea"; 
+</script>
+
+<script type="text/javascript"
+src="http://www.statcounter.com/counter/counter.js"></script><noscript><div
+class="statcounter"><a title="vBulletin analytics"
+href="http://statcounter.com/vbulletin/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/702220/0/92a002ea/1/"
+alt="vBulletin analytics" ></a></div></noscript>
+<!-- End of StatCounter Code -->
+</body>
+
+</html>
